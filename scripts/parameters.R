4c716b17849d1c2a4b71e62d8cd85b3e52afb651
PhilErm <philerm@gmail.com>
2019-03-08 16:51:29 +1100

    Changed values in repository parameters script.

--- scripts/parameters.R
+++ scripts/parameters.R
@@ -2,7 +2,7 @@
 # The parameters for the model
 
 # Invasion parameters
-N <- 1500 # The number of individuals in the initial population
+N <- 300 # The number of individuals in the initial population
 nSpread <- 3 # The number of patches invaders the initial population is spread over
 m <- 0.5 # The probability of an individual dispersing
 
@@ -11,12 +11,12 @@ r <- 0.2 # The intrinsic growth rate of individuals
 K <- 500 # The carrying capacity of each patch
 
 # Trait parameters
-A.mean <- 250 # The mean around which initial invaders' Allee traits are distributed
+A.mean <- 0 # The mean around which initial invaders' Allee traits are distributed
 A.sd <- 10 # The SD of the distribution of initial invaders' Allee traits
 
 # Evolution parameters
-mut.prob <- 0.1 # Probability a given individual will mutate
-mut.sd <- 100 # SD of the Allee trait's mutation
+mut.prob <- 0.01 # Probability a given individual will mutate
+mut.sd <- 10 # SD of the Allee trait's mutation
 
 # Miscellaneous parameters
 ex.prob <- 0 # The probability of any given patch being randomly wiped out
